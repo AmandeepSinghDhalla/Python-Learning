223e6319b55d639404e957b50786373f48ec916a
Amandeep Singh <64694891+AmandeepSinghDhalla@users.noreply.github.com>
2020-11-19 13:22:10 +0530

    Update rockpaperscissor.py

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -17,41 +17,34 @@ while i < 10:
         print("Match Draw!")
         print()
         i += 1
-    elif comp == 'Rock' and user == 'Paper':
+    elif (comp == 'Rock' or user == 'Rock') and (user == 'Paper' or comp == 'Paper'):
         print(f"Computer played {comp}")
-        uw += 1
-        print(f"You won. Score Computer: {cw} Player: {uw}")
+        if comp == 'Rock':
+            uw += 1
+            print(f"You won. Score Computer: {cw} Player: {uw}")
+        elif user == 'Rock':
+            cw += 1
+            print(f"Computer won. Score Computer: {cw} Player: {uw}")
         print()
         i += 1
-    elif user == 'Rock' and comp == 'Paper':
+    elif (comp == 'Rock' or user == 'Rock') and (user == 'Scissor' or comp == 'Scissor'):
         print(f"Computer played {comp}")
-        cw += 1
-        print(f"Computer won. Score Computer: {cw} Player: {uw}")
+        if comp == 'Rock':
+            cw += 1
+            print(f"Computer won. Score Computer: {cw} Player: {uw}")
+        elif user == 'Rock':
+            uw += 1
+            print(f"You won. Score Computer: {cw} Player: {uw}")
         print()
         i += 1
-    elif comp == 'Rock' and user == 'Scissor':
+    elif (user == 'Paper' or comp == 'Paper') and (comp == 'Scissor' or user == 'Scissor'):
         print(f"Computer played {comp}")
-        cw += 1
-        print(f"Computer won. Score Computer: {cw} Player: {uw}")
+        if comp == 'Paper':
+            uw += 1
+            print(f"You won. Score Computer: {cw} Player: {uw}")
+        elif user == 'Paper':
+            cw += 1
+            print(f"Computer won. Score Computer: {cw} Player: {uw}")
         print()
         i += 1
-    elif user == 'Rock' and comp == 'Scissor':
-        print(f"Computer played {comp}")
-        uw += 1
-        print(f"You won. Score Computer: {cw} Player: {uw}")
-        print()
-        i += 1
-    elif user == 'Paper' and comp == 'Scissor':
-        print(f"Computer played {comp}")
-        cw += 1
-        print(f"Computer won. Score Computer: {cw} Player: {uw}")
-        print()
-        i += 1
-    elif comp == 'Paper' and user == 'Scissor':
-        print(f"Computer played {comp}")
-        uw += 1
-        print(f"You won. Score Computer: {cw} Player: {uw}")
-        print()
-        i += 1
-
 print(f"Final Score Computer: {cw} Player: {uw}")
